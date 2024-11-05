e37ffd82327f6e0bd0d24fc23cae18f708a6c9d9
amirradjou <amir.radical.78@gmail.com>
2024-11-05 15:56:22 -0500

    updated Encode function including CID generation

--- oplog/entry.go
+++ oplog/entry.go
@@ -1,44 +1,56 @@
 package oplog
 
 import (
-    "bytes"
-    "github.com/ipld/go-ipld-prime"
-    "github.com/ipld/go-ipld-prime/node/bindnode"
-    "github.com/ipld/go-ipld-prime/codec/dagcbor"
+	"bytes"
+	"github.com/ipfs/go-cid"
+	"github.com/ipld/go-ipld-prime"
+	"github.com/ipld/go-ipld-prime/codec/dagcbor"
+	"github.com/ipld/go-ipld-prime/node/bindnode"
+	mh "github.com/multiformats/go-multihash"
 )
 
 type Entry struct {
-    Payload string
+	Payload string
 }
 
 type EncodedEntry struct {
-    Entry
-    Bytes bytes.Buffer
+	Entry
+	Bytes bytes.Buffer
+	CID   cid.Cid
 }
 
 func NewEntry(payload string) EncodedEntry {
-    entry := Entry{Payload: payload}
-    return Encode(entry)
+	entry := Entry{Payload: payload}
+	return Encode(entry)
 }
 
 func Encode(entry Entry) EncodedEntry {
-    ts, err := ipld.LoadSchemaBytes([]byte(`
-        type Entry struct {
-            payload String
-        } representation tuple
-    `))
-    if err != nil {
-        panic(err)
-    }
+	// Define the schema and encode the entry to CBOR format
+	ts, err := ipld.LoadSchemaBytes([]byte(`
+		type Entry struct {
+			payload String
+		} representation tuple
+	`))
+	if err != nil {
+		panic(err)
+	}
 
-    
-    schemaType := ts.TypeByName("Entry")
-    node := bindnode.Wrap(&entry, schemaType)   
+	schemaType := ts.TypeByName("Entry")
+	node := bindnode.Wrap(&entry, schemaType)
 
-    var buf bytes.Buffer
-    dagcbor.Encode(node.Representation(), &buf)
-    
-    encodedEntry := EncodedEntry{Entry: entry, Bytes: buf}
-        
-    return encodedEntry
+	var buf bytes.Buffer
+	if err := dagcbor.Encode(node.Representation(), &buf); err != nil {
+		panic(err)
+	}
+
+	// Hash the bytes and generate a CID
+	hash, err := mh.Sum(buf.Bytes(), mh.SHA2_256, -1) // SHA-256 hash
+	if err != nil {
+		panic(err)
+	}
+
+	c := cid.NewCidV1(cid.DagCBOR, hash) // Create CID with DAG-CBOR codec
+
+	// Return the EncodedEntry with CID
+	return EncodedEntry{Entry: entry, Bytes: buf, CID: c}
 }
