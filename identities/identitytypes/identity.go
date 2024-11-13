2f7646f13cef0e255072d3a0ef3cd09f654575e6
amirradjou <amir.radical.78@gmail.com>
2024-11-13 12:21:49 -0500

    Added Signature, Bytes and Type to identity structure

--- identities/identitytypes/identity.go
+++ identities/identitytypes/identity.go
@@ -15,22 +15,47 @@ type Identity struct {
 	PublicKey  string            // Hex representation of the public key
 	PrivateKey *ecdsa.PrivateKey // ECDSA private key for signing
 	Hash       string            // Hash of the identity (ID + PublicKey)
+	Signatures map[string]string // Signatures for id and publicKey
+	Bytes      []byte            // Encoded byte representation of the identity
+	Type       string
+}
+
+// EncodedIdentity represents an Identity that has been encoded.
+type EncodedIdentity struct {
+	Identity Identity
+	Bytes    []byte
+	CID      cid.Cid
+	Hash     string
 }
 
 // NewIdentity generates a new Identity instance with a random ECDSA key pair.
-func NewIdentity(id string, privateKey *ecdsa.PrivateKey) (*Identity, error) {
+func NewIdentity(id string, privateKey *ecdsa.PrivateKey, idType string) (*Identity, error) {
 	if id == "" {
 		return nil, errors.New("identity ID is required")
 	}
+	if idType == "" {
+		return nil, errors.New("identity type is required")
+	}
+
 	publicKey := hex.EncodeToString(append(privateKey.PublicKey.X.Bytes(), privateKey.PublicKey.Y.Bytes()...))
 
-	hash := sha256.Sum256([]byte(id + publicKey))
-	return &Identity{
+	identity := &Identity{
 		ID:         id,
 		PublicKey:  publicKey,
 		PrivateKey: privateKey,
-		Hash:       hex.EncodeToString(hash[:]),
-	}, nil
+		Signatures: make(map[string]string),
+		Type:       idType, // Set the type here
+	}
+
+	// Encode the identity to generate hash and bytes representation
+	hash, bytes, err := EncodeIdentity(*identity)
+	if err != nil {
+		return nil, err
+	}
+	identity.Hash = hash
+	identity.Bytes = bytes
+
+	return identity, nil
 }
 
 // Sign generates a signature for the provided data using the private key.
