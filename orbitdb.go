6f6c4322628fae2693216916cb9013b07c6d7a83
amirradjou <amir.radical.78@gmail.com>
2024-11-13 16:47:17 -0500

    Added a test for check identity hash consistency

--- orbitdb.go
+++ orbitdb.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"log"
+	"orbitdb/go-orbitdb/identities/identitytypes"
 	"orbitdb/go-orbitdb/oplog"
 )
 
@@ -25,4 +27,32 @@ func main() {
 	fmt.Println("CID in Go:", encodedEntry.CID)
 	// Output the bytes in hex format for easy comparison
 	fmt.Println("CBOR Encoded Bytes in Go:", hex.EncodeToString(encodedEntry.Bytes))
+
+	// Define the test identity details
+	id := "0x01234567890abcdefghijklmnopqrstuvwxyz"
+	publicKey := "<pubkey>"
+	signatures := map[string]string{
+		"id":        "signature for <id>",
+		"publicKey": "signature for <publicKey + idSignature>",
+	}
+	idType := "orbitdb"
+
+	// Create an Identity struct without a PrivateKey
+	testIdentity := identitytypes.Identity{
+		ID:         id,
+		PublicKey:  publicKey,
+		Signatures: signatures,
+		Type:       idType,
+	}
+
+	// Encode the identity to get the CBOR-encoded bytes and CID hash
+	hash, encodedBytes, err := identitytypes.EncodeIdentity(testIdentity)
+	if err != nil {
+		log.Fatalf("Error encoding identity: %v", err)
+	}
+
+	// Display the encoded values
+	fmt.Printf("Encoded Hash: %s\n", hash)
+	fmt.Printf("Encoded Bytes (CBOR): %x\n", encodedBytes)
+	// expected hash: 'zdpuArx43BnXdDff5rjrGLYrxUomxNroc2uaocTgcWK76UfQT'
 }
